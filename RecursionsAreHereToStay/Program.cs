bed416332f3fe5fc43fd31d991b806dc14df6f39
Home <Home@LAPTOP-2E6V6ACM>
2023-11-16 10:41:24 +0200

    ForClass

--- RecursionsAreHereToStay/Program.cs
+++ RecursionsAreHereToStay/Program.cs
@@ -1,15 +1,24 @@
-﻿namespace RecursionsAreHereToStay
+﻿
+namespace RecursionsAreHereToStay
 {
     internal class Program
     {
 
+        /// <summary>
+        /// מחלקה המיצגת שקית קליק.
+        /// אנחנו לא יודעים כמה כדורים יש בשקית
+        /// פעולות:
+        /// 1. האם השקית ריקה?
+        /// לקחת כדור אחד מהשקית.2
+        /// </summary>
         public class Klik
         {
             private int balls;
+
             static Random rnd=new Random();
             public Klik()
             {
-                balls=rnd.Next(100);
+                balls=rnd.Next(10);
             }
 
             public bool IsEmpty()
@@ -27,20 +36,39 @@
 
         }
 
+        /// <summary>
+        /// בתהליך רגיל- נרוץ בלולאה עד שהשקית מתרוקנת
+        /// 
+        /// </summary>
+        /// <param name="klik"></param>
+        /// <returns></returns>
         public static int HowMany(Klik klik)
         {
             int counter = 0;
             while (!klik.IsEmpty())
             {
-                klik.TakeOne();    
-                counter++; }
+               counter+= klik.TakeOne();    
+                }
             return counter;
         }
 
+        /// <summary>
+        /// התהליך הרקורסיבי בנוי על כך שבמקום לולאה מתבצעות קריאות
+        /// בכל פעם אותה קריאה רק עם בעיה יותר קטנה.
+        /// הקריאה הקודמת לא מסתיימת עד שמתקבלת תשובה מהקריאה הבאה
+        /// והתהליך כאשר הגענו לבעיה שיש לנו עליה פתרון
+        /// ואנו מתבססים על הפתרון הזה כדי להחזיר תשובה לכל אחת מהקריאות 
+        /// 
+        /// 
+        /// </summary>
+        /// <param name="klik"></param>
+        /// <returns></returns>
         public static int HowManyRecursive(Klik klik)
         {
+            //תנאי עצירה - השקית ריקה
             if(klik.IsEmpty())
-                return 0;
+                return 0;//יודעים בוודאות שבשקית ריקה יש 0 קליקים
+            //צעד הרקורסיה- זה הקליק שהוצאתי  + כמה שנשאר בשקית
             return klik.TakeOne() +HowManyRecursive(klik);
         }
        
@@ -48,7 +76,7 @@
         {
             
             Klik klik = new Klik();
-            Console.WriteLine(  HowManyRecursive(klik));
+            Console.WriteLine(HowManyRecursive(klik));
 
         }
     }
---
+++ RecursionsAreHereToStay/RecurssionSamples.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecursionsAreHereToStay
+{
+    //מחחקה סטטית כי כל הפעולות הן סטטיות
+    public static class RecurssionSamples
+    {
+
+        #region פעולות רקורסיביות סופרות
+        /// <summary>
+        /// פעולה הסופרת מספר ספרות
+        /// אם המספר קטן מ-10 המשמעות היא שמדובר במספר בעל ספרה 1=תנאי העצירה
+        /// כל עוד יש ספרות במספר- נספור את הספרה הנוכחית + כמות הספרות שיש בלי הספרה האחרונה
+
+        /// </summary>
+        /// <param name="num"></param>
+        /// <returns></returns>
+        public static int CountDigitsV1(int num)
+        {
+            if (Math.Abs(num) < 10)
+                return 1;
+            return 1 + CountDigitsV1(num / 10);
+        }
+
+        /// <summary>
+        /// על אותו עיקרון אם כאשר נקצץ את הספרה האחרונה נקבל 0
+        /// המשמעות היא שקיימת רק ספרה אחת במספר - תנאי עצירה
+        /// </summary>
+        /// <param name="num"></param>
+        /// <returns></returns>
+        public static int CountDigitsV2(int num)
+        {
+            if (Math.Abs(num)/10 == 0)
+                return 1;
+            return 1 + CountDigitsV2(num / 10);
+        }
+        #endregion
+
+
+        #region פעולות רקורסיביות סוכמות
+        /// <summary>
+        /// תנאי עצירה - כאשר הגענו לספרה בודדת
+        /// סכום של ספרה בודדת הוא הספרה עצמה!
+        /// כל עוד לא הגענו לתנאי עצירה 
+        /// הסכום הוא ספרת האחדות + הסכום הספרות של שאר המספר
+        /// </summary>
+        /// <param name="num"></param>
+        /// <returns></returns>
+        public static int SumDigits(int num)
+        {
+            if (Math.Abs(num) < 10)
+                return num;
+            return num%10 + SumDigits(num / 10);
+        }
+        #endregion
+
+        #region פעולות בוליאניות
+        //א. לבדוק שתמיד יש לנו 
+        //לפחות אחד מכל אופציה
+        //return true;
+        //return false;
+        //ב. לא לשכוח קריאה רקורסיבית
+
+        public static bool IsExistV1(int num,int digit)
+        {
+            //אם ספרה בודדת- נחזיר אמת אם היא שווה לספרה ושקר אחרת
+            if (num / 10 == 0)
+                return num == digit;
+            //אם המספר יותר מספרה אחת, נבדוק האם ספרת האחדות זהה לספרה שמחפשים
+            if (num % 10 == digit)
+                return true;
+            //אם לא זהה- נמשיך לחפש בשאר המספר
+            return IsExistV1(num/10, digit);  
+        }
+        public static bool IsExistV2(int num,int digit)
+        {
+            //אם ספרה בודדת- נחזיר אמת אם היא שווה לספרה ושקר אחרת
+            if (num / 10 == 0)
+                return num == digit;
+            //אם המספר יותר מספרה אחת, נבדוק האם ספרת האחדות זהה לספרה שמחפשים
+            //או שהיא קיימת בשאר המספר
+
+            //חשבו-----> האם משנה אם נהפוך את סדר הבדיקה בתנאים
+            //האם יש הבדל מהשורה הבאה?
+            //return (IsExistV2(num/10, digit)||(num%10==digit) );
+
+            return ((num%10==digit) || IsExistV2(num/10, digit));
+        }
+        #endregion
+
+        #region שאלת מעקב
+        //מה הבעיה הלוגית בפעולה הבאה
+        //בצעו מעקב עבור המספרים 359
+        //426
+        //132
+        //מה הפעולה עושה
+        public static bool Kuku(int num)
+        {
+            if (num / 10 == 0)
+                return true;
+            if (num / 10 % 2 == num % 2)
+                return true;
+            return Kuku(num / 10);
+        }
+
+        #endregion
+
+    }
+}
